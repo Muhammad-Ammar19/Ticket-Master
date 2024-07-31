beddef169316ca7d44aeae70ce4ac10d57d4f882
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-07-31 19:24:31 +0500

    .

---
+++ ticket_master/lib/Screens/main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TicketMaster extends StatelessWidget {
+  const TicketMaster({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
